0ec5845a6cda293a0fd0a65acaa19ddd7aa9daff
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2020-11-25 15:56:23 +0000

    Added more debugging

--- tasks/cleanup.sh
+++ tasks/cleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 sudo -u pe-puppet -H bash -c "rm -rf $PT_code_staging/$PT_environment"
 rm -f $PT_signature_location/$PT_environment/$PT_commit_hash.jwt
--- tasks/r10k_deploy.sh
+++ tasks/r10k_deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -x
 
 sudo -u pe-puppet -H bash -c "/opt/puppetlabs/puppet/bin/r10k deploy environment -c /opt/puppetlabs/server/data/code-manager/r10k.yaml -p -v debug $PT_environment"
